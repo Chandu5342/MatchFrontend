0df748ddeba7e90362b770bd2f1db0de7eb64fcd
Chandu5342 <karrichandu309@gmail.com>
2025-12-31 23:47:51 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">
+        Sports Matches Platform 🚀
+      </h2>
+    </div>
+  );
+}
+
+export default App;
